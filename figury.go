ce5756f1dd22f8f7610171298c01ec5277cfba46
Andrzej Gajowniczek <andrzej.gajowniczek@gmail.com>
2023-03-27 00:05:23 +0200

    funkcja pole i obwód trójkąta na podstawie długości 3 boków

--- figury.go
+++ figury.go
@@ -1,37 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
 
-type trójkąt struct{
+type trójkąt struct {
 	a float64
-	h float64
+	b float64
+	c float64
 	o float64
-    p float64
+	p float64
 }
 
-type kwadrat struct{
+func (t trójkąt) pole() (float64, float64) {
+	t.o = t.a + t.b + t.c
+
+	t.p = math.Sqrt(t.o / 2 * (t.o/2 - t.a) * (t.o/2 - t.b) * (t.o/2 - t.c))
+	return t.p, t.o
+}
+
+type kwadrat struct {
 	a float64
 	o float64
 	p float64
 }
 
-type prostokąt struct{
-
+type prostokąt struct {
 	a float64
 	b float64
 	o float64
 	p float64
 }
-type koło struct{
+type koło struct {
 	r float64
 	p float64
 	o float64
 }
 
-var okrąg = koło
+func main() {
 
+	var T trójkąt
+	T = trójkąt{
+		a: 2,
+		b: 2,
+		c: 2,
+	}
 
-main() {
-
-
+	pol, obw := T.pole()
+	fmt.Println("Pole trójkąta o bokach:", T.a, T.b, T.c, "wynosi w jednostkach kwadratowych:", pol, "a obwód w jednostkach liniowych:", obw)
 
 }
